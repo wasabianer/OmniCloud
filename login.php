6dfe0ff5c87ff00a6dc329650da9bddc8724a026
wasabi <fabio@trosi.li>
2023-12-07 19:58:04 +0100

    Fix bug in login functionality

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-In</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <input type="submit" value="Log-In">
+    </form>
+    <a href="register.php">Register</a>
+    
+</body>
+</html>
