5a39bc60ad0b0ec1c38e90f9070a538ef8a6c020
wasabi <fabio@trosi.li>
2023-12-07 20:55:13 +0100

    Fix formatting in index.php

--- index.php
+++ index.php
@@ -17,12 +17,13 @@
                 <li><a href="#home">Home</a></li>
                 <li style="float:right"><a class="active" href="./Login/register.php">Registrieren</a></li>
             </ul>
+        </nav>
     </header>
 
 
 
-        <br><br>
-        <form action="Formular\test.php" method=post>
+    <br><br>
+    <form action="Formular\test.php" method=post>
         <!-- <form action="Formular\formular.php" method=post>-->
 
         <fieldset>
@@ -31,45 +32,45 @@
             <input type="number" name="k_id" id="k_id" placeholder="123456" required><br>
 
         </fieldset>
-            <fieldset>
-                <legend>Angaben zum Server ausfüllen</legend>
-                <label for="cpu">Bitte Cpuanzahl auswählen:</label> <br>
-                <select name="cpu" id="cpu">
-                    <option value="1">1 (5 CHF)</option>
-                    <option value="2">2 (10 CHF)</option>
-                    <option value="4">4 (18 CHF)</option>
-                    <option value="8">8 (30 CHF)</option>
-                    <option value="16">16 (45 CHF)</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <label for="ram">Bitte Arbeitsspeicher auswählen:</label> <br>
-                <select name="ram" id="ram">
-                    <option value="512">512 (5 CHF)</option>
-                    <option value="1024">1024 (10 CHF)</option>
-                    <option value="2048">2048 (20 CHF)</option>
-                    <option value="4096">4096 (40 CHF)</option>
-                    <option value="8192">8192 (80 CHF)</option>
-                    <option value="16384">16384 (160 CHF)</option>
-                    <option value="32768">32768 (320)</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <label for="ssd">Bitte Speicherplatz(SSD) auswählen:</label> <br>
-                <select name="ssd" id="ssd">
-                    <option value="10">10 (5 CHF)</option>
-                    <option value="20">20 (10 CHF)</option>
-                    <option value="40">40 (20 CHF)</option>
-                    <option value="80">80 (40 CHF)</option>
-                    <option value="240">240 (80 CHF)</option>
-                    <option value="500">500 (160 CHF)</option>
-                    <option value="1000">1000 (320)</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <input type="submit" name="absenden" value="Send">
-            </fieldset>
-        </form>
+        <fieldset>
+            <legend>Angaben zum Server ausfüllen</legend>
+            <label for="cpu">Bitte Cpuanzahl auswählen:</label> <br>
+            <select name="cpu" id="cpu">
+                <option value="1">1 (5 CHF)</option>
+                <option value="2">2 (10 CHF)</option>
+                <option value="4">4 (18 CHF)</option>
+                <option value="8">8 (30 CHF)</option>
+                <option value="16">16 (45 CHF)</option>
+            </select>
+        </fieldset>
+        <fieldset>
+            <label for="ram">Bitte Arbeitsspeicher auswählen:</label> <br>
+            <select name="ram" id="ram">
+                <option value="512">512 (5 CHF)</option>
+                <option value="1024">1024 (10 CHF)</option>
+                <option value="2048">2048 (20 CHF)</option>
+                <option value="4096">4096 (40 CHF)</option>
+                <option value="8192">8192 (80 CHF)</option>
+                <option value="16384">16384 (160 CHF)</option>
+                <option value="32768">32768 (320)</option>
+            </select>
+        </fieldset>
+        <fieldset>
+            <label for="ssd">Bitte Speicherplatz(SSD) auswählen:</label> <br>
+            <select name="ssd" id="ssd">
+                <option value="10">10 (5 CHF)</option>
+                <option value="20">20 (10 CHF)</option>
+                <option value="40">40 (20 CHF)</option>
+                <option value="80">80 (40 CHF)</option>
+                <option value="240">240 (80 CHF)</option>
+                <option value="500">500 (160 CHF)</option>
+                <option value="1000">1000 (320)</option>
+            </select>
+        </fieldset>
+        <fieldset>
+            <input type="submit" name="absenden" value="Send">
+        </fieldset>
+    </form>
 
     <footer>
 
