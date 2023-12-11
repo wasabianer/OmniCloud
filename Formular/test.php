d6562a25a2ba1d76b3b6c26d11b51be3f03c70d8
Astro2006 <135111039+Astro2006@users.noreply.github.com>
2023-12-11 11:59:21 +0100

    Add files via upload

--- Formular/test.php
+++ Formular/test.php
@@ -6,7 +6,7 @@ $ram = 32768;
 $ssd = 2000;
 $k_id = 6666;
 */
-$oldprice = 0;
+
 
 //Variabeln
 $cpu = $_POST["cpu"];
@@ -20,7 +20,6 @@ $filepath = "..\Log\server\server_cs.txt";
 $fail_path = "..\Log\server_fail.txt";
 $user_path = "..\Log\user\user_".$k_id .".txt";
 $userlogpath = "..\Log\user_changes\user_log.txt";
-
 
 //Funktionen
 function getPrice($cpu, $ram, $ssd){
@@ -108,6 +107,88 @@ function choose_Server($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1){
     }
 }
 
+function UpdateServer($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1, $s_id, $old_extract){
+    
+    
+
+    //Update Server
+    $s_id_old = $old_extract[1];
+    $cpu_old = $old_extract[2];
+    $ram_old = $old_extract[3];
+    $ssd_old = $old_extract[4];
+    $old_price = $old_extract[5];
+    $check = $old_extract[6];
+    $s_id = choose_Server($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1);
+    $newcpu = $cpu_old - $cpu;
+    $newram = $ram_old - $ram;
+    $newssd = $ssd_old - $ssd;
+    //Solange der alte Server nicht ersetzt werden muss kann der alte Server weiter verwendet werden.
+    if($check == "-"){ 
+        echo "neu". "<br>";
+        switch ($s_id) {
+            case 1:
+                $small_get1[1] = $small_get1[1] - $cpu;
+                $small_get1[2] = $small_get1[2] - $ram;
+                $small_get1[3] = $small_get1[3] - $ssd;
+                break;
+            case 2:
+                $medium_get1[1] = $medium_get1[1] - $cpu;
+                $medium_get1[2] = $medium_get1[2] - $ram;
+                $medium_get1[3] = $medium_get1[3] - $ssd;
+                break;
+            case 3:
+                $big_get1[1] = $big_get1[1] - $cpu;
+                $big_get1[2] = $big_get1[2] - $ram;
+                $big_get1[3] = $big_get1[3] - $ssd;
+                break;
+        }
+        echo print_r($small_get1) . "<br>";   
+        echo print_r($medium_get1). "<br>"; 
+        echo print_r($big_get1). "<br>";
+    
+    }
+    else if($check = "changed"){
+        if ($s_id_old == $s_id){
+            echo "gleich". "<br>";
+            switch ($s_id_old) {
+                case 1:
+                    $small_get1[1] = $small_get1[1] + $newcpu;
+                    $small_get1[2] = $small_get1[2] + $newram;
+                    $small_get1[3] = $small_get1[3] + $newssd;
+                    break;
+                case 2:
+                    $medium_get1[1] = $medium_get1[1] + $newcpu;
+                    $medium_get1[2] = $medium_get1[2] + $newram;
+                    $medium_get1[3] = $medium_get1[3] + $newssd;
+                    break;
+                case 3:
+                    $big_get1[1] = $big_get1[1] + $newcpu;
+                    $big_get1[2] = $big_get1[2] + $newram;
+                    $big_get1[3] = $big_get1[3] + $newssd;
+                    break;
+            }
+        echo print_r($small_get1) . "<br>";   
+        echo print_r($medium_get1). "<br>"; 
+        echo print_r($big_get1). "<br>"; 
+    }else {
+//  Swicht zwischen verschiedenen Servern
+        echo"fehler";
+
+    }
+    
+
+    }else{
+    echo "<br>". "Test Fail". "<br>";
+    }
+
+    }
+    
+
+
+
+
+
+
 //überprüfung der eingabe Serverseitig
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["k_id"]) && isset($_POST["absenden"]) && !empty(trim(is_numeric($_POST["k_id"])))) {
@@ -132,28 +213,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["k_id"]) && isset($_POS
 
         //Code um alle leerzeichen zu löschen
 
-       $user_path = "..\Log\user\user_".$k_id.".txt";
-
         if(file_exists($user_path)) {
-           $old_get = file_get_contents($user_path);
+           
+            $old_get = file_get_contents($user_path);
             $old_extract = explode(";", $old_get);
-            if ($old_extract[6] == "changed") {
 
-               
-               
+            if ($old_extract[6] == "changed") {
+                $s_id = $old_extract[1];
+                $oldprice = $old_extract[5];
+                //UpdateServer($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1, $s_id, $old_extract);
+
             }
 
         }else{
             
-          
-          $s_id = 3;
+          $s_id = choose_Server($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1);
 
         }
+
+        //Funktion um die Recurcen zu berechnen und von den Servern abzuziehen
+        //UpdateServer($cpu, $ram, $ssd, $small_get1, $medium_get1, $big_get1, $s_id, $user_path);
 
         //create one file per user
         $price = getPrice($cpu, $ram, $ssd);
         if (file_exists($user_path)) {
             $s_id = $old_extract[1];
+            $oldprice = $old_extract[5];
             $newprice = $oldprice + $price;
            file_put_contents($user_path, $k_id . ";" . $s_id . ";" . $cpu . ";" . $ram . ";" . $ssd . ";" . $newprice . ";" . "changed" . ";");
             // user log überschreibung der alten daten 
@@ -175,8 +260,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["k_id"]) && isset($_POS
     } else {
         echo "<br>" . "Kein Zugriff auf die Server Daten" . "<br>";
     }
-    
-
 
 } else {
     // Fail Log
